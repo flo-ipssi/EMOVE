93433645782347bf3060877447d06374860be23d
flo-ipssi <florian.emmanuel@outlook.fr>
2018-06-21 16:28:45 +0200

    Ajout du Dashboard V1.0 + Modal

--- eMove/index.php
+++ eMove/index.php
@@ -5,25 +5,27 @@
 	<title>eMove - Location de voiture et scooter</title>
 	<link rel="stylesheet" href="css/bootstrap.min.css" >
 	<link rel="stylesheet" href="css/font-awesome.min.css" >
+	<link rel="stylesheet" href="css/owl.carousel.min.css"> 
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
 	<link rel="stylesheet" type="text/css" href="css/style.css" />
 	<script src="js/jquery-3.3.1.min.js"></script>
-	
+
 </head>
 <body>
 	<header>
 		<nav  class="container">
 			<a href="/"><img src="img/logo.png" /></a>
 			<ul>
-				<li><a href="">Inscription</a></li>
+				<li><button type="button"  data-toggle="modal" data-target="#insription">Inscription</button></li>
 				<li> | </li>
-				<li><a href="">Connexion</a></li>
-				<li><a href=""><img src="img/user.png" alt="" /></a></li>
+				<li><button type="button"  data-toggle="modal" data-target="#connexion">Connexion</button></li>
+				<li><a href="#"><img src="img/user.png" alt="" /></a></li>
 			</ul>
 		</nav>
 	</header>
 	<div class="container-fluid">
 		<div id="carrousel" class="carousel row slide" data-ride="carousel">
-			<div class="search">
+			<div class="search container">
 			<h2>Avec eMove, louer votre véhicule facilement et rapidement</h2>
 				<form class="form">
 					<div class="row">
@@ -63,7 +65,7 @@
 				<h2>Près de 2 agences de location de voitures en France</h2>
 				<h2>Qualité et Service sont nos priorités</h2>
 			</div>
-			<div class="row" >
+			<div class="summary">
 				<div class="card  col-xs-8 col-sm-8 col-md-3 ">
 					<i class="fa fa-building-o card-img" aria-hidden="true"></i>
 				  	<div class="card-img-overlay">
@@ -86,6 +88,76 @@
 				</div>
 
 			</div>
+		</div>
+		<hr>
+		<div class="promo">
+			<h2>Actualité</h2>
+			<div class="owl-carousel">
+			  	<div> 
+			  		<div class="card" >
+				  	<img class="card-img-top" src="img/vintage.jpg" alt="Card image cap">
+				  	<div class="card-body">
+				    	<h5 class="card-title">Card title</h5>
+				    	<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				    <a href="#" class="btn btn-primary">Go somewhere</a>
+				  	</div>
+					</div>
+			  </div>
+			  	<div> 
+			  		<div class="card" >
+				  	<img class="card-img-top" src="img/vintage.jpg" alt="Card image cap">
+				  	<div class="card-body">
+				    	<h5 class="card-title">Card title</h5>
+				    	<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				    <a href="#" class="btn btn-primary">Go somewhere</a>
+				  	</div>
+					</div>
+			  </div>
+			  	<div> 
+			  		<div class="card" >
+				  	<img class="card-img-top" src="img/vintage.jpg" alt="Card image cap">
+				  	<div class="card-body">
+				    	<h5 class="card-title">Card title</h5>
+				    	<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				    <a href="#" class="btn btn-primary">Go somewhere</a>
+				  	</div>
+					</div>
+			  </div>
+			  	<div> 
+			  		<div class="card" >
+				  	<img class="card-img-top" src="img/vintage.jpg" alt="Card image cap">
+				  	<div class="card-body">
+				    	<h5 class="card-title">Card title</h5>
+				    	<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				    <a href="#" class="btn btn-primary">Go somewhere</a>
+				  	</div>
+					</div>
+			  </div>
+			  	<div> 
+			  		<div class="card" >
+				  	<img class="card-img-top" src="img/vintage.jpg" alt="Card image cap">
+				  	<div class="card-body">
+				    	<h5 class="card-title">Card title</h5>
+				    	<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				    <a href="#" class="btn btn-primary">Go somewhere</a>
+				  	</div>
+					</div>
+			  </div>
+			  	<div> 
+			  		<div class="card" >
+				  	<img class="card-img-top" src="img/vintage.jpg" alt="Card image cap">
+				  	<div class="card-body">
+				    	<h5 class="card-title">Card title</h5>
+				    	<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				    <a href="#" class="btn btn-primary">Go somewhere</a>
+				  	</div>
+					</div>
+			  </div>
+			</div>
+		</div>
+		<div class="map">
+			<h2>Où nous trouver ?</h2>
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2625.507672328738!2d2.3861542159290914!3d48.8485289093547!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6720ccc2ca7b1%3A0x45528eb90c108f61!2sIPSSI+Paris+%2F+Groupe+IP-Formation!5e0!3m2!1sfr!2sfr!4v1529315928827"  height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 		</div>
 		<div class="contact">
 			<form class="row" action="envoi.php" method="POST">
@@ -134,28 +206,29 @@
 			<hr>
 		</div>
 	</div>
-		<!-- Footer -->
-		<footer class="page-footer font-small cyan darken-3 mt-4">
+		
+	<!-- Footer -->
+		<footer class="small-footer font-small cyan darken-3 mt-4">
 		  <div class="container">
 		    <div class="row">
 		      <div class="col-md-12 py-5">
 		        <div class="mb-5 flex-center">
-		          <a class="fb-ic">
+		          <a href="" class="fb-ic">
 		            <i class="fa fa-facebook fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
 		          </a>
-		          <a class="tw-ic">
+		          <a href="" class="tw-ic">
 		            <i class="fa fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
 		          </a>
-		          <a class="gplus-ic">
+		          <a href="" class="gplus-ic">
 		            <i class="fa fa-google-plus fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
 		          </a>
-		          <a class="li-ic">
+		          <a href="" class="li-ic">
 		            <i class="fa fa-linkedin fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
 		          </a>
-		          <a class="ins-ic">
+		          <a href="" class="ins-ic">
 		            <i class="fa fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
 		          </a>
-		          <a class="pin-ic">
+		          <a href="" class="pin-ic">
 		            <i class="fa fa-pinterest fa-lg white-text fa-2x"> </i>
 		          </a>
 		        </div>
@@ -165,10 +238,12 @@
 		  <div class="footer-copyright text-center py-3">© 2018 Copyright 
 		    <a href="https://mdbootstrap.com/bootstrap-tutorial/"> eMove.com</a>
 		  </div>
-		</footer>
-		<!-- Footer -->
-	
-	<script src="js/jquery.parallax-1.1.3.js" ></script>
+		</footer>	
+	<!-- Footer -->
+	<!-- Modal -->
+  	<?php require 'blocs/modal.php' ?>
+  	<!-- Modal-->
+	<script src="js/owl.carousel.min.js"></script>
 	<script src="js/api.js" ></script>
 	<script src="js/popper.min.js" ></script>
 	<script src="js/bootstrap-datepicker.min.js" ></script>
